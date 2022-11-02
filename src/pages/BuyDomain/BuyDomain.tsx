92abb2adcc4a4688ce5d557eb9584469287a4722
Brett <12437916+colbr@users.noreply.github.com>
2022-11-02 19:54:58 +1300

    feat(buy-domain): remove wallet requirement

--- src/pages/BuyDomain/BuyDomain.tsx
+++ src/pages/BuyDomain/BuyDomain.tsx
@@ -1,9 +1,6 @@
 import type { FC } from 'react';
 import { useState } from 'react';
 
-import { useWeb3 } from '../../lib/hooks';
-
-import { ConnectWallet } from '../../features/ui/ConnectWallet';
 import { IconZero } from '../../features/buy-domain/Icons';
 import { DomainSearch } from '../../features/buy-domain/DomainSearch';
 import { BuyDomainModal } from '../../features/buy-domain/BuyDomainModal';
@@ -11,8 +8,6 @@ import { BuyDomainModal } from '../../features/buy-domain/BuyDomainModal';
 import styles from './BuyDomain.module.scss';
 
 export const BuyDomain: FC = () => {
-	const { account } = useWeb3();
-
 	const [domainToPurchase, setDomainToPurchase] = useState<
 		string | undefined
 	>();
@@ -24,18 +19,6 @@ export const BuyDomain: FC = () => {
 	const onModalClose = () => {
 		setDomainToPurchase(undefined);
 	};
-
-	if (!account) {
-		return (
-			<div className={styles.Container}>
-				<div className={styles.Wrapper}>
-					<ConnectWallet
-						message={'Connect a Web3 wallet to see your Buy Domains data.'}
-					/>
-				</div>
-			</div>
-		);
-	}
 
 	return (
 		<>
